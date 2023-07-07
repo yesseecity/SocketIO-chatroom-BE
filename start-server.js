272e0245aada0a94b6d15e43de1128951ce3401e
yesseecity <yesseecity@yahoo.com>
2023-07-08 00:34:43 +0800

    Serving static files in Express

--- start-server.js
+++ start-server.js
@@ -1,29 +1,20 @@
 const http = require('http');
-// const app = require('./express-app');
+const app = require('./express-app');
 const socketio = require('socket.io');
 const uniqid = require('uniqid');
 
 const port = 8080;
-const webServer = http.createServer(
-  (req, res)=>{
-    // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Request-Method', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    if ( req.method === 'OPTIONS' ) {
-      res.writeHead(200);
-      res.end();
-      return;
-    }
-  }
-);
+const webServer = http.createServer();
 webServer.listen(port, ()=>{
   console.log('web socker server started!');
 });
-// webServer.on('request', app);
+webServer.on('request', app);
 
-const io = socketio(webServer);
+const io = socketio(webServer, {
+  cors: {
+    origin: '*',
+  }
+});
 
 io.on('connection', (socket) => {
   console.log(socket.id, ' connected');
@@ -56,44 +47,4 @@ io.on('connection', (socket) => {
     io.to(msgObj.roomId).emit('new message', msg);
   });
 
-  socket.on('client message', function(msg){
-    console.log('client message');
-    console.log(msg)
-    return;
-    let msgObj = JSON.parse(msg);
-    let roomId = msgObj.roomid;
-    let room = io.sockets.adapter.rooms[roomId]
-    delete msgObj['roomid']
-    if (room == undefined) {
-      return
-    }
-    let roomSockets = room.sockets;
-    for (let id in roomSockets) {
-      if (id !== socket.id) {
-        io.to(id).emit('client msg',  JSON.stringify(msgObj))
-      }
-    }
-    
-    // Emit message to all sockets in room
-    // io.to(roomId or socketId ).emit('host message', 'host emit message')
-  });
-
-  socket.on('host message', function(msg){
-    console.log('host message')
-    console.log(msg)
-    return
-    let msgObj = JSON.parse(msg);
-    let roomId = msgObj.roomid;
-    let room = io.sockets.adapter.rooms[roomId]
-    delete msgObj['roomid']
-    if (room == undefined) {
-      return
-    }
-    let roomSockets = room.sockets
-    for (let id in roomSockets) {
-      if (id !== socket.id) {
-        io.to(id).emit('host msg', JSON.stringify(msgObj))
-      }
-    }
-  });
 });
